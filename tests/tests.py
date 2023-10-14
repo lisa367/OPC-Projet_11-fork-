80e6a257083aa31471c028e3ae1fa75115c69157
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-10-14 23:45:59 +0200

    conftest  import

--- tests/tests.py
+++ tests/tests.py
@@ -1,66 +1,7 @@
 import pytest
 from ..server import create_app, competitionDateFilter, book, purchasePlaces
+from conftest import *
 
-
-@pytest.fixture
-def client():
-    app = create_app({"TESTING": True})
-    with app.test_client() as client:
-        yield client
-
-
-@pytest.fixture
-def clubs_data():
-    clubs =  [
-        {
-            "name": "Simply Lift",
-            "email": "john@simplylift.com",
-            "points": "13"
-        },
-        {
-            "name": "Iron Temple",
-            "email": "admin@irontemple.com",
-            "points": "4"
-        },
-        {
-            "name": "She Lifts",
-            "email": "kate@shelifts.co.uk",
-            "points": "8"
-        }
-    ]
-    return clubs
-
-@pytest.fixture
-def competitions_data():
-    competitions = [
-        {
-            "name": "Spring Festival",
-            "date": "2020-03-27 10:00:00",
-            "numberOfPlaces": "24"
-        },
-        {
-            "name": "Fall Classic",
-            "date": "2023-10-22 13:30:00",
-            "numberOfPlaces": "11"
-        },
-        {
-            "name": "Winter Competition",
-            "date": "2024-10-22 13:30:00",
-            "numberOfPlaces": "30"
-        },
-        {
-            "name": "Spring Festival",
-            "date": "2024-03-27 10:00:00",
-            "numberOfPlaces": "25"
-        },
-        {
-            "name": "Summer Classic",
-            "date": "2024-10-22 13:30:00",
-            "numberOfPlaces": "18"
-        }
-    ]
-
-    return competitions
 
 clubs = clubs_data()
 competitions = competitions_data()
