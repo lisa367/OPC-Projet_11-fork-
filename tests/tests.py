dd5fc2885a313b1031f5a35b346c3b1361a79d23
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-10-19 16:45:35 +0200

    test for bug 4 - test_showSummary_past_competition

--- tests/tests.py
+++ tests/tests.py
@@ -8,7 +8,7 @@ competitions = loadCompetitions()
 
 
 # Bug 1
-def test_showSummary(client):
+def test_showSummary_wrong_email(client):
     """Test for bug #1
     Checks that a wrong email address doesn't raise an error
 
@@ -19,13 +19,36 @@ def test_showSummary(client):
 
     # assert response.data != IndexError
     assert response.status_code == 200
+
+
+# Bug 4
+def test_showSummary_past_competition(client):
+    """Test for bug #1
+    Checks that a there is no link to book a place in a past competition
+
+    Args:
+        client (_type_): _description_
+    """
+    response = client.post("/showSummary", data={"email": "fake.email@test.com"})
+
+    # assert response.data != IndexError
+    assert response.status_code == 200
     assert b"Mail not found" in response.data
+    paragraph = b"""
+        <li>
+            Spring Festival<br />
+            Date: 2020-03-27 10:00:00</br>
+            Number of Places: 24  
+        </li>
+"""
+    assert paragraph in response.data
 
 
 # Bug 2
-def test_competitionDateFilter():
+def test_competitionDateFilter(client):
     """ assert competitionDateFilter("2000-01-01") == False
     assert competitionDateFilter("2050-01-01") == True """
+    response = client.get()
     assert 1 == 1
 
 
