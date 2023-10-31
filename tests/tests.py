64276d8c633751cad246d3b67ad9fb7bab2e38e6
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-10-31 06:05:31 +0100

    test_points_deduction - tests for bug #5 updated

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,7 @@
 import pytest
-# from server import competitionDateFilter
-from .conftest import client, getClubs, getCompetitions
+from .conftest import client
 
 
-""" clubs = getClubs()
-competitions = getCompetitions() """
 
 def test_index_page(client):
     """
@@ -66,66 +63,3 @@ def test_points_deduction(client):
 
     assert b"Points available: 8" in response_1.data
     assert b"Points available: 6" in response_2.data
-
-
-
-'''
-clubs = getClubs()
-competitions = getCompetitions()
-
-# Bug 5
-def test_points_deduction(client):
-    """Test for bug #5
-    Checks that the points used to purchase places are deducted from the total in the database
-    """
-    points_before_transaction = int(clubs[2]["points"])
-    response = client.post("/purchasePlaces", data={"competition": ["Summer Classic"], "club": ["She Lifts"], "places": 2})
-    points_after_transaction = int(clubs[2]["points"]) 
-    places_purchased = response.data[2]
-    assert points_after_transaction == points_before_transaction - places_purchased
-
-
-def test_max_purchasePlaces(client):
-    competitions = {}
-    """Test for bug #3
-    Prevent the user from buying more than 12 places per competition
-
-    Args:
-        client (_type_): _description_
-    """
-    response = client.post(
-        "/purchasePlaces", data={"club": "She Lifts", "competition": "Spring Festival"}
-    )
-
-'''
-
-
-
-""" def test_competitionDateFilter(client):
-    assert competitionDateFilter("2000-01-01") == False
-    assert competitionDateFilter("2050-01-01") == True
-    response = client.get()
-    assert 1 == 1
-
-
-def test_book():
-    pass """
-
-
-
-# test du bug 4 pour tester l'affichage du template
-""" def test_valid_competion():
-    pass
- """
-
-'''
-def test_login_with_wrong_email(client):
-    """Test for bug #1
-    Checks that a wrong email address doesn't raise an error
-
-    Args:
-        client (_type_): _description_
-    """
-    response = client.post("/showSummary", data={"email": "fake.email@test.com"})
-    assert response.status_code == 200
-'''
